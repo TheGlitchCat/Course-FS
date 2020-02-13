bfb14c3ced46ad19db499f35a8c7caada289d515
TheGlitchCat <jorgesebasah@hotmail.com>
2020-02-12 21:32:14 -0500

    adding package.json

--- Clase 2 - Nodejs/Modules/http.js
+++ Clase 2 - Nodejs/Modules/http.js
@@ -1,4 +1,35 @@
 const http = require('http');
-
+/*
 const server = http.createServer();
 
+server.on('connection', (socket) => {
+    console.log('New connection established');
+})
+
+server.listen(2020);
+console.log('listen on 2020 port');*/
+/*
+const server = http.createServer((req, res) =>{
+    if(req.url === '/'){
+        res.write('Hello World');
+        res.write('from Nodejs');
+        res.end();
+    }else{
+        res.write('forbidden 401');
+        res.end()
+    }
+}).listen(2020);
+
+console.log('Listen on 2020');
+*/
+
+
+
+const server = http.createServer((req, res) =>{
+    res.writeHead(200, {'Content-type':'text/html'});
+
+    res.write('<h1>200 Successful</h1>');
+    res.end();
+
+}).listen(2020);
+console.log('listen on 2020');
