bf6d93d754401b5a4956c117e9e35100da7499da
TheGlitchCat <jorgesebasah@hotmail.com>
2020-02-13 16:07:02 -0500

    update project

--- Clase 2 - Nodejs/express/index.js
+++ Clase 2 - Nodejs/express/index.js
@@ -5,4 +5,9 @@ app.get('/', (req, res) => {
     res.send('Hello world from express');
 });
 
+app.get('/api/cars', (req, res) =>{
+    res.send(['BMW','Audi','Ferrari','Mazda','Fiat']);
+});
+
+
 app.listen(2020);
